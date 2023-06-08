6609c1eb25a56d2c035342620f3118d39327d075
Vicrrs <enricofermi2019@gmail.com>
2023-06-08 01:14:12 -0400

    lendo e salvando imagem

---
+++ scripts/Introducao/lendo_e_salvando_img.py
@@ -0,0 +1,18 @@
+import cv2
+
+# caminho da imagem
+IMG_PATH = r"C:\Users\rozas\Projects\opencv-experiments\imgs\Anumara_Vision.png"
+
+img = cv2.imread(IMG_PATH)
+
+# cria a janela
+cv2.imshow("Janela", img)
+
+# mostra a janela criada e aguarda o evento de uma tecla
+k = cv2.waitKey(0)
+
+# se s for pressionado salva a imagem
+if k == ord("s"):
+    cv2.imwrite("modificado.jpg", img)
+
+cv2.destroyAllWindows()
