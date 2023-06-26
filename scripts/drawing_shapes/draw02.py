51155c48c6495dd3f92b1103ab7f6a94e2d80f84
Vicrrs <enricofermi2019@gmail.com>
2023-06-26 15:49:20 -0400

    draw02

---
+++ scripts/drawing_shapes/draw02.py
@@ -0,0 +1,20 @@
+import cv2 as cv
+import numpy as np
+
+RED = (0, 0, 255)
+YELLOW = (0, 255, 255)
+
+p0, p1, p2 = (10, 10), (300,90), (400,10)
+
+img = img = np.zeros((100, 500, 3), np.uint8)
+cv.line(img, p0, p1, RED, 2)
+cv.line(img, p1, p2, YELLOW, 5)
+cv.imshow("RGB", img)
+
+gray_img = np.zeros((100, 50), np.uint8)
+cv.line(gray_img, p0, p1, 100, 2)
+cv.line(gray_img, p1, p2, 255, 5)
+cv.imshow("Gray", gray_img)
+
+cv.waitKey(0)
+cv.destroyAllWindows()
